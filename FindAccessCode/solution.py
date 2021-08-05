fde8f42c917bb5f84862709dc1f8a6659cf00a5b
Faiz Alqorni <faiz.alqorni@m2.formulatrix.com>
2021-08-05 19:34:34 +0700

    solution run, but not right

--- FindAccessCode/solution.py
+++ FindAccessCode/solution.py
@@ -1,2 +1,29 @@
-def​ ​solution(l):
-​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
+def factorial(n):
+    if n<2:
+        return 1
+    else :
+        return n*factorial(n-1)
+
+def combination(item, take):
+    combination = factorial(item)/(take*factorial(item-take))
+    return int(combination)
+
+def solution(l):
+    l = [1,2,3,4,5,6]
+    test2 = [1,1,1]
+    count = 0
+    divisible = [0]*len(l)
+    
+    for index_divisor, divisor in enumerate(l):
+        for index_dividend, dividend in enumerate(l[index_divisor:]):
+            if dividend%divisor == 0:
+                divisible[index_dividend] += 1
+        
+    for div_num in divisible:
+        if div_num < 2:
+            continue
+        else:
+            count += combination(div_num, 2)
+    return count  
+    
+print(solution(0))
--- FindAccessCode/testings.py
+++ FindAccessCode/testings.py
@@ -1,2 +1,11 @@
-def​ ​solution(l):
-​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
+def factorial(n):
+    if n<2:
+        return 1
+    else :
+        return n*factorial(n-1)
+
+def combination(item, take):
+    combination = factorial(item)/(take*factorial(item-take))
+    return combination
+    
+print(combination(10,2))
