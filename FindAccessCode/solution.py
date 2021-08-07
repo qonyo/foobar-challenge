dfe8c2d20c1ff16dfbbea37b56f0dc4c0c9c03c1
Faiz Alqorni <faiz.alqorni@m2.formulatrix.com>
2021-08-08 00:04:10 +0700

    Fix non sorted list

--- FindAccessCode/solution.py
+++ FindAccessCode/solution.py
@@ -5,18 +5,15 @@ def solution(l):
     if len(l) <= 2:
         return 0
     
-    div_check = list(set(comb for comb in combinations(l,2)))
+    div_check = list(set(comb for comb in combinations(sorted(l),2)))
     unique_count = dict(Counter(l))
     divisors_count = dict.fromkeys(unique_count.keys(), 0)
     multipliers_count =  dict.fromkeys(unique_count.keys(), 0)
     triples_total = 0
-    for check in div_check:
-        if check[0] % check[1] == 0:
-            divisors_count[check[0]] += 1
-            multipliers_count[check[1]] += 1
-        elif check[1] % check[0] == 0:
-            divisors_count[check[1]] += 1
-            multipliers_count[check[0]] += 1
+    for x,y in div_check:
+        if y % x == 0:
+            divisors_count[y] += 1
+            multipliers_count[x] += 1
     
     for member in unique_count:
         if unique_count[member] == 2:
@@ -25,4 +22,4 @@ def solution(l):
         triples_total += (divisors_count[member]*multipliers_count[member])
     return triples_total
     
-print(solution([1,1,1]))
+print(solution([8,2,4,2,4]))
