a92754811e21b175a8d54fe01c1c59679ff1d358
Faiz Alqorni <faiz.alqorni@m2.formulatrix.com>
2021-08-07 23:07:33 +0700

    prepare new approach

--- FindAccessCode/solution.py
+++ FindAccessCode/solution.py
@@ -9,17 +9,8 @@ def combination(item, take):
     return int(combination)
 
 def solution(l):
-    l = [1,2,2,4]
-    count = 0
-    divisible = [0]*len(l)
-    for index_divisor, divisor in enumerate(l):
-        for index_dividend, dividend in enumerate(l[index_divisor+1:]):
-            if dividend%divisor == 0:
-                divisible[index_dividend + index_divisor +1] += 1  
-    for div_num in divisible:
-        if div_num < 2:
-            continue
-        else:
-            count += div_num -1
-    return count
+    if len(l) <= 2:
+        return 0
+    
+    
 print(solution(0))
