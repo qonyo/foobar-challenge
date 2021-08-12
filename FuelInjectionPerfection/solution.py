dc0d83f03022440bb9f0a068d12732ae23a6ef76
Faiz Alqorni <faiz.alqorni@m2.formulatrix.com>
2021-08-12 11:41:31 +0700

    working solution

--- FuelInjectionPerfection/solution.py
+++ FuelInjectionPerfection/solution.py
@@ -1,3 +1,12 @@
-def​ ​solution(n):
-​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
+def solution(n):
+    if n==1:
+        return 0
+    elif n%2 ==0:
+        return 1+solution(n/2)
+    elif n%8 == 7:
+        return 1+solution(n+1)
+    else:
+        return 1+solution(n-1)
 
+print(solution(4))
+print(solution(15))
