b5c9a8e6c3e76495ffc01a33f46806fb3fa94537
qonyo <faizqornijo@gmail.com>
2021-08-12 20:58:22 +0700

    I will let it rest

--- FuelInjectionPerfection/solution.py
+++ FuelInjectionPerfection/solution.py
@@ -1,14 +1,18 @@
 def solution(n):
     count = 0
-    while n != 1:
+    while n > 1:
         if n&1 ==0:
             n >>= 1
         elif n&7 == 7:
+            print(n)
             n += 1
         else:
             n-=1
         count += 1
     return count
 
-print(solution(4))
-print(solution(15))
+print("solution : ",solution(4))
+print("solution : ",solution(15))
+#print(solution(10**309 - 1))
+print("solution : ",solution(3))
+print("solution : ",solution(39))
