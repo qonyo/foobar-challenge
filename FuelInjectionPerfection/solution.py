20192105a1b7be67fa52c98399a9021eeea2a9bc
Faiz Alqorni <faiz.alqorni@m2.formulatrix.com>
2021-08-12 11:55:30 +0700

    found out looping is faster than recursion

--- FuelInjectionPerfection/solution.py
+++ FuelInjectionPerfection/solution.py
@@ -1,12 +1,14 @@
 def solution(n):
-    if n==1:
-        return 0
-    elif n%2 ==0:
-        return 1+solution(n/2)
-    elif n%8 == 7:
-        return 1+solution(n+1)
-    else:
-        return 1+solution(n-1)
+    count = 0
+    while n != 1:
+        if n%2==0:
+            n /= 2
+        elif n%8 == 7:
+            n += 1
+        else:
+            n-=1
+        count += 1
+    return count
 
 print(solution(4))
 print(solution(15))
