dc8c9a0388c05f1381b9cf8ee6ff6fa218790547
qonyo <faizqornijo@gmail.com>
2021-08-13 17:30:53 +0700

    accepted solution

--- FuelInjectionPerfection/solution.py
+++ FuelInjectionPerfection/solution.py
@@ -1,18 +1,17 @@
 def solution(n):
     count = 0
+    n = int(n)
     while n > 1:
         if n&1 ==0:
             n >>= 1
-        elif n&7 == 7:
-            print(n)
-            n += 1
+        elif n%4 ==1 or n==3:
+            n -= 1
         else:
-            n-=1
+            n += 1
         count += 1
     return count
 
 print("solution : ",solution(4))
 print("solution : ",solution(15))
-#print(solution(10**309 - 1))
 print("solution : ",solution(3))
 print("solution : ",solution(39))
