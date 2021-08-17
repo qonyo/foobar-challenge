9e4733cd0e697eeaefaf9fcb4120b07c28563ea6
qonyo <faizqornijo@gmail.com>
2021-08-17 16:27:16 +0700

    solution worked assuming pickaxe is not needed, start integrating pickaxe

--- PrepareBunnyEscape/solution.py
+++ PrepareBunnyEscape/solution.py
@@ -1,48 +1,72 @@
 import time
-import copy
+import queue
 
-LENGTH = 0
-WIDTH = 0
+D_COL = [-1,0,1,0]
+D_ROW = [0,-1,0,1]
+VISITED = list()
+GRID = list()
+ROW_QUE = queue.Queue()
+COL_QUE = queue.Queue()
+PICKAXE_QUE = queue.Queue()
+NODES_NEXT_LAYER = 0
+def neighbours_lookup(row_now, col_now, pax_now):
+    global NODES_NEXT_LAYER, ROW_QUE, COL_QUE, PICKAXE_QUE, VISITED
+    for i in range(4):
+        row_next = row_now + D_ROW[i]
+        col_next = col_now + D_COL[i]
+    
+        if row_next < 0 or col_next < 0 or row_next >= HEIGHT or col_next >= WIDTH: continue
+        if VISITED[row_next][col_next]: continue
+        if GRID[row_next][col_next] == 1 : continue
+        
+        ROW_QUE.put(row_next)
+        COL_QUE.put(col_next)
+        PICKAXE_QUE.put(pax_now)
+        VISITED[row_next][col_next] += 1
+        NODES_NEXT_LAYER += 1
+        
+def solution(grid):
+    global VISITED, WIDTH, HEIGHT, GRID, NODES_NEXT_LAYER
+    GRID = grid
+    WIDTH = len(GRID[0])
+    HEIGHT = len(GRID)
+    source = (0,0,True) #third column for breaker
+    target = (HEIGHT-1, WIDTH-1) #row, col target
+    VISITED = [[False for col in range(WIDTH)] for row in range(HEIGHT)]
+    depth = 1
+    
+    ROW_QUE.put(source[0])
+    COL_QUE.put(source[1])
+    PICKAXE_QUE.put(source[2])
+    nodes_in_layer = 1
+    while not(ROW_QUE.empty()):
+        row_now = ROW_QUE.get()
+        col_now = COL_QUE.get()
+        pax_now = PICKAXE_QUE.get()
+        VISITED[row_now][col_now] = True
+        if row_now == target[0] and col_now == target[1]:
+            return depth
+        
+        neighbours_lookup(row_now, col_now, pax_now)
+        nodes_in_layer-=1
+        
+        if nodes_in_layer == 0:
+            nodes_in_layer = NODES_NEXT_LAYER
+            NODES_NEXT_LAYER = 0
+            depth+=1
+    
+    return -1
 
-def explore(maps, breaker, x, y):
-    
-    if x < 0 or y < 0 or x >= WIDTH or y >= LENGTH:
-        return 1000
-    
-    if x == WIDTH-1 and y == LENGTH-1:
-        return 1
-    
-    val = maps[y][x]
-    journal = copy.deepcopy(maps)
-    journal[y][x] = -1
-    
-    if val == 1 :
-        if breaker :
-            new_breaker = False
-            return min(1 + explore(journal, new_breaker, x-1, y),
-                       1 + explore(journal, new_breaker, x+1, y),
-                       1 + explore(journal, new_breaker, x, y-1),
-                       1 + explore(journal, new_breaker, x, y+1),)
-        
-        return 1000
-        
-    elif val == -1:
-        return 1000
-        
-    else:
-        return min(1 + explore(journal, breaker, x-1, y),
-                   1 + explore(journal, breaker, x+1, y),
-                   1 + explore(journal, breaker, x, y-1),
-                   1 + explore(journal, breaker, x, y+1),)
 
-def solution(maps):
-    global LENGTH, WIDTH
-    breaker = True
-    x,y = (0,0)
-    LENGTH = len(maps)
-    WIDTH = len(maps[0])
-    count = explore(maps ,breaker, x, y)
-    return count
+start = time.time()    
+print(solution([
+[0, 0, 0], 
+[0, 0, 0], 
+[0, 0, 0], 
+[0, 0, 0]
+]))
+end = time.time()
+print(end-start)
 
 start = time.time()    
 print(solution([
@@ -76,6 +100,19 @@ print(solution([
 [0, 1, 1, 1, 1, 1], 
 [0, 0, 0, 0, 0, 0]
 ]
+))
+end = time.time()
+print(end-start)
+
+start = time.time() 
+print(solution([
+[0, 0, 0, 0, 0, 0],
+[1, 1, 1, 1, 1, 0],
+[1, 1, 1, 1, 1, 1],
+[0, 0, 0, 0, 0, 0],
+[0, 1, 1, 1, 1, 1],
+[0, 0, 0, 0, 0, 0]
+] 
 ))
 end = time.time()
 print(end-start)
